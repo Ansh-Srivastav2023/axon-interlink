37175b60efedf24aba5496d265455521af45b91d
Ansh-Srivastav2023 <anshs.ec.23@nitj.ac.in>
2026-06-19 14:04:12 +0530

    Initial commit

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ git add .
 git commit -m "Initial commit"
 
 git branch -M main
-git remote add origin https://github.com/<username>/<repo>.git
+git remote add origin https://github.com/Ansh-Srivastav2023/axon-interlink.git
 git push -u origin main
